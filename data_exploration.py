58f846a217c3e6d4825ab14a6bfe339ea9e12088
“jackson_gilkey” <jackson.gilkey@gmail.com>
2020-01-15 10:06:57 -0500

    table creation and insert for salary data

--- data_exploration.py
+++ data_exploration.py
@@ -2,14 +2,15 @@ import data_frame_creation
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from statsmodels.formula.api import ols
+import statsmodels.api as sm
+from statsmodels.stats.multicomp import pairwise_tukeyhsd
+from statsmodels.stats.multicomp import MultiComparison
 
 df = data_frame_creation.create_combine_data_frame()
 
-#df = df.dropna()
-
 pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
-# print(df.columns)
+#pd.set_option('display.max_rows', None)
 
 def columns_to_float(df, column_list):
 
@@ -17,48 +18,56 @@ def columns_to_float(df, column_list):
         df[c] = df[c].astype(float)
     return df
 
-df_numbers = df.loc[:, ['height', 'weight', '40yd', 'vertical', 'bench', 'broad_jump', '3cone', 'shuttle']]
-#print(df_numbers)
-df_numbers = columns_to_float(df_numbers,['height', 'weight', '40yd','vertical', 'bench', 'broad_jump', '3cone', 'shuttle'])
-print(df_numbers.dtypes)
+df = columns_to_float(df,['height', 'weight', '40yd','vertical', 'bench', 'broad_jump', '3cone', 'shuttle'])
+
+
+def create_position_groupings(row):
+    position_dict = {
+    'DB' : ['CB', 'FS', 'SS', 'S', 'DB'],
+    'OL' : ['OT', 'OG', 'C', 'OL'],
+    'DL' : ['DE', 'DT', 'DL'],
+    'LB' : ['OLB', 'ILB', 'EDGE', 'LB'],
+    'ST' : ['K', 'P', 'LS'],
+    'SKILL' : ['RB', 'FB', 'WR', 'TE'],
+    'QB' : ['QB']}
+
+    for key in position_dict:
+        if row['position'] in position_dict[key]:
+            return key
 
 
 
-# df_numbers = df.loc[:, ['height', 'weight', '40yd',
-#        'vertical', 'bench', 'broad_jump', '3cone', 'shuttle', 'pick_number']]
-
-line_men = ['OT', 'OG', 'DT', 'C', 'NT', 'OL', 'DL', 'DE']
-skill_men  = ['OLB', 'RB', 'TE', 'CB', 'K', 'P', 'FS', 'ILB','SS', 'QB', 'WR', 'FB', 'LS','EDGE', 'S', 'LB', 'DB']
-
-
+df['position_grouping'] = df.apply(lambda row: create_position_groupings(row), axis =1)
 
 def hist_plot(df, to_plot):
+    #takes a dataframe and a column to create a hist plot on
+    df = df.dropna(subset = [to_plot])
     fig = plt.figure()
     plt.style.use('seaborn')
     sns.set_palette('colorblind')
     hist_serial = sns.distplot(df[to_plot].astype(float), kde = False)
-    #print(stats.kurtosis(list(df['trees_per_sq_mile'])))
-    #print(stats.skew(list(df['trees_per_sq_mile'])))
     #hist_serial.set_xlabel('Trees Per Sq Mile')
     #hist_serial.set_xlabel('Trees per sq Mile')
     plt.show()
 
 #hist_plot(df, 'weight')
-
 #hist_plot(df, 'height')
+#hist_plot(df_numbers, '3cone')
 
-def simple_box(df):
+
+def simple_box_by_position(df, to_plot):
+    df = df.dropna(subset = [to_plot])
     fig = plt.figure()
     plt.style.use('seaborn')
     sns.set_palette('colorblind')
-    ax = sns.boxplot(x= 'position', y = 'weight', data = df)
+    ax = sns.boxplot(x= 'position_grouping', y = to_plot, data = df)
 
-    #ax.set_ylabel('Median Home Value')
-    ax.set_ylabel('Trees per sq Mile')
-    ax.set_xlabel('Borough')
+    # ax.set_ylabel('Median Home Value')
+    # ax.set_ylabel('Trees per sq Mile')
+    # ax.set_xlabel('Borough')
     plt.show()
 
-#simple_box(df)
+#simple_box_by_position(df, 'weight')
 
 def pair_plot(df):
     fig = plt.figure()
@@ -70,4 +79,27 @@ def pair_plot(df):
     sns.heatmap(corr, xticklabels=corr.columns, yticklabels=corr.columns, annot=True)
     plt.show()
 
-pair_plot(df_numbers)
+def anova_by_position(df, to_plot):
+    df = df.dropna(subset = [to_plot])
+    anova_pos = ols((to_plot + '~position_grouping'), data=df).fit()
+    anova_table = sm.stats.anova_lm(anova_pos, type=2)
+    return anova_table
+
+def multi_by_position(df, to_plot):
+    df = df.dropna(subset = [to_plot, 'position_grouping'])
+    mc = MultiComparison(df[to_plot], df['position_grouping'])
+    mc_results = mc.tukeyhsd()
+    return mc_results
+
+
+#print(df['position_grouping'].unique())
+
+# for i in df['position_grouping'].unique():
+#     print(i)
+#     print(df[df['position_grouping'] == i]['weight'].describe())
+    #print(df[df['position_grouping'] == i].mean)
+
+print(multi_by_position(df, 'height'))
+#print(df[])
+#print(df[df['position_grouping'].isnull()])
+#pair_plot(df_numbers)
