e28f66513b10665847fb3077db0b5a14c3fe85fc
Andrew Triola <andrewtriola@gmail.com>
2020-01-16 12:12:37 -0500

    regression

--- position_breakdown.py
+++ position_breakdown.py
@@ -25,8 +25,9 @@ df_joined = df_joined.set_index('id')
 df_joined = df_joined.drop(df_joined.columns[0], axis=1)
 
 ## create position group map
+## remove QB and ST players
 
-position_groups = {'WR': 'BK', 'DE': 'DL', 'OLB': 'LB', 'RB': 'BK', 'OT': 'OL', 'CB': 'BK', 'QB': 'QB',
+position_groups = {'WR': 'BK', 'DE': 'DL', 'OLB': 'LB', 'RB': 'BK', 'OT': 'OL', 'CB': 'BK', 'QB': 'ST',
                   'FS': 'BK', 'DT': 'DL', 'C': 'OL', 'P': 'ST', 'TE': 'BK', 'SS': 'BK', 'ILB': 'LB', 'OG': 'OL',
                   'EDGE': 'LB', 'S': 'BK', 'FB': 'BK', 'LB': 'LB', 'DB': 'BK', 'OL': 'OL', 'DL': 'DL',
                   'K': 'ST'}
@@ -38,6 +39,16 @@ cols = cols[:5] + cols[-1:] + cols[5:-1]
 cols.pop(7)
 df_joined = df_joined[cols]
 
-#### add dummies 
-position_dummy = pd.get_dummies(df_joined['pos_grp'],prefix = 'position', drop_first=True)
-df_with_dummies = pd.concat([df_joined, position_dummy], axis = 1)
+df_joined2 = df_joined[(df_joined['pos_grp'] != 'ST')]
+
+### create log_salary column
+df_joined2['avg_salary'] = df_joined2.apply(lambda row: row['total_value']/row['contract_years'], axis=1)
+df_joined2['log_salary'] = df_joined2.apply(lambda row: np.log(row['avg_salary']), axis =1)
+
+#### add dummies
+position_dummy = pd.get_dummies(df_joined2['pos_grp'],prefix = 'position', drop_first=True)
+df_with_dummies = pd.concat([df_joined2, position_dummy], axis = 1)
+
+### potential logistic regression model
+log_reg_ = ols('log_salary~forty+shuttle+bench+position_DL+position_LB+position_OL', data=df_with_dummies).fit()
+log_reg_.summary()
